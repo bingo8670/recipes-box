184c1b05a900bc1baf7b4b215b4decb59a0c6e49
bingo8670 <307286130@qq.com>
2017-07-21 21:56:39 +0800

    event简单排序

--- app/models/event.rb
+++ app/models/event.rb
@@ -24,4 +24,7 @@ class Event < ApplicationRecord
  has_many :tickets, :dependent => :destroy
  accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
 
+ include RankedModel
+ ranks :row_order
+
 end
