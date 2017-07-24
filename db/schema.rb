e04635924995320277508d20fb0bc1c47e28219e
bingo8670 <307286130@qq.com>
2017-07-24 21:58:07 +0800

    百宝箱完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20170723014016) do
 
   create_table "categories", force: :cascade do |t|
